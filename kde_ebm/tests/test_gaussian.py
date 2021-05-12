a1e009d59bf0081244f711300472a85825829064
Neil Oxtoby <n.oxtoby@ucl.ac.uk>
2021-05-12 15:36:05 +0100

    Python 2 WTF

--- kde_ebm/tests/test_gaussian.py
+++ kde_ebm/tests/test_gaussian.py
@@ -12,12 +12,12 @@ def main():
     module_loc = os.path.abspath(os.path.join(floc, '../../'))
     X, y = pkl.load(open('%s/data/normal_mixture.pkl' % (module_loc)))
     # for i in range(X.shape):
-    # print X.shape, y.shape
+    # print('{0}, {1}'.format(X.shape, y.shape))
     g = Gaussian.Gaussian(mu=0, sigma=1)
-    # print g.pdf([0, 0.1, 0.5, 1])[0] == 0.39894228
+    # print(g.pdf([0, 0.1, 0.5, 1])[0] == 0.39894228)
     event_sign = np.nanmean(X[y == 0, 0]) > np.nanmean(X[y == 1, 0])
-    print np.nanmin(X[y == 0])
-    print g.get_bounds(X[:, 0], X[y == 0], event_sign)
+    print(np.nanmin(X[y == 0]))
+    print(g.get_bounds(X[:, 0], X[y == 0], event_sign))
 
 
 if __name__ == '__main__':
