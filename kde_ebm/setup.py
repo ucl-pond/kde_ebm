8b33e38123ec4703aed2dfc38c6b1e20a2850950
Neil Oxtoby <n.oxtoby@ucl.ac.uk>
2025-01-10 22:15:41 +0000

    Update setup.py for setuptools
    No more distutils (if I got it right)

--- kde_ebm/setup.py
+++ kde_ebm/setup.py
@@ -1,26 +1,10 @@
-
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-
-    config = Configuration('kde_ebm', parent_package, top_path)
-
-    # submodules which do not have their own setup.py
-    # we must manually add sub-submodules & tests
-    config.add_subpackage('distributions')
-    config.add_subpackage('event_order')
-    config.add_subpackage('mcmc')
-    config.add_subpackage('mixture_model')
-    config.add_subpackage('plotting')
-
-    # submodules which have their own setup.py
-    config.add_subpackage('datasets')
-
-    # add the test directory
-    config.add_subpackage('tests')
-
-    return config
+def setup_package():
+    from setuptools import setup, find_packages
+    metadata = dict(name='kde_ebm',
+                    packages=find_packages()
+                    )
+    setup(**metadata)
 
 
-if __name__ == '__main__':
-    from numpy.distutils.core import setup
-    setup(**configuration(top_path='').todict())
+if __name__ == "__main__":
+    setup_package()
