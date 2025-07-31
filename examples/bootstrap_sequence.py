483010dc484454842433e706d9a3096a10f09235
Neil Oxtoby <n.oxtoby@ucl.ac.uk>
2025-07-31 16:05:44 +0100

    bootstrap_sequence.py working

--- examples/bootstrap_sequence.py
+++ examples/bootstrap_sequence.py
@@ -1,23 +1,24 @@
 # Authors: Nicholas C. Firth <ncfirth87@gmail.com>
 # License: TBC
-from ebm import mixture_model
-from ebm import mcmc
-from ebm import plotting
-from ebm import datasets
-from ebm import distributions
+from kde_ebm import mixture_model
+from kde_ebm import mcmc
+from kde_ebm import plotting
+from kde_ebm import datasets
+from kde_ebm import distributions
 from matplotlib import pyplot as plt
 
 
 def main():
     X, y, bmname, cname = datasets.load_synthetic('synthetic_1500_10.csv')
-    mixture_models = []
-    for i in range(X.shape[1]):
-        h_model = distributions.Gaussian()
-        d_model = distributions.Gaussian()
-        gmm = mixture_model.MixtureModel(cn_comp=h_model,
-                                         ad_comp=d_model)
-        gmm.fit(X[:, i], y)
-        mixture_models.append(gmm)
+    mixture_models = mixture_model.fit_all_kde_models(X, y)
+    # mixture_models = []
+    # for i in range(X.shape[1]):
+    #     h_model = distributions.Gaussian()
+    #     d_model = distributions.Gaussian()
+    #     gmm = mixture_model.MixtureModel(cn_comp=h_model,
+    #                                      ad_comp=d_model)
+    #     gmm.fit(X[:, i], y)
+    #     mixture_models.append(gmm)
     fig, ax = plotting.mixture_model_grid(X, y, mixture_models,
                                           score_names=bmname,
                                           class_names=cname)
@@ -33,9 +34,12 @@ def main():
     bs_samples = mcmc.bootstrap_ebm(X, y, n_mcmc_iter=200,
                                     n_bootstrap=10, greedy_n_init=2,
                                     greedy_n_iter=10)
-    fig, ax = plotting.mcmc_uncert_mat(bs_samples, ml_order=ml_order,
-                                       score_names=bmname)
-    fig.show()
+    for bs_k,bs in enumerate(bs_samples):
+        fig, ax = plotting.mcmc_uncert_mat(bs, ml_order=ml_order,
+                                           score_names=bmname)
+        ax.set_title(f"Bootstrap {bs_k+1}")
+        fig.tight_layout()
+        fig.show()
     plt.show()
 
 
